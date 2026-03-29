5f65cf47b56906638a320e5a88fe3303bbb76ec4
electrospot105 <adithyanvs105@gmail.com>
2026-03-29 15:03:28 +0530

    feat: add Vercel entry point with directory initialization and error reporting

--- api/index.php
+++ api/index.php
@@ -5,6 +5,11 @@
  * This is the entry point for Vercel functions.
  */
 
+// TEMPORARY: Enable error reporting for debugging 500 error
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (isset($_SERVER['VERCEL_URL'])) {
     $storage = [
         '/tmp/storage/framework/cache',
